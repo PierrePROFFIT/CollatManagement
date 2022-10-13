d871e08058502c1f6d06839d8d0ce94c88818c48
PierrePROFFIT <pierre37.proffit@gmail.com>
2022-10-13 11:16:22 +0200

    2e

---
+++ main.py
@@ -0,0 +1,14 @@
+## Importing app (ie pages)
+from lib.app import main_app
+
+## Importing libraries
+import streamlit as st
+
+#st.set_page_config(layout="wide")
+pages = {
+        "Run Strategies":main_app
+        }
+st.sidebar.title("Collateral-Manager")
+select_page = st.sidebar.radio("GO TO : ", list(pages.keys()))
+page = pages[select_page]
+page.app()
